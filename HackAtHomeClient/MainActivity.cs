4e31c8eddda7bff111e14bbfdba6e401bd4ed699
geh <germaneherrera@gmail.com>
2017-06-30 09:52:31 -0500

    first commit

---
+++ HackAtHomeClient/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HackAtHomeClient
+{
+    [Activity(Label = "Hack@Home", MainLauncher = true, Icon = "@drawable/iconAndroid", Theme = "@android:style/Theme.Holo")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
